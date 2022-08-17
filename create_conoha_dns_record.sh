7260a97353b48bfcaefe1d79da1ea107a40425ae
TOGURO Mikito <mit@shalab.net>
2022-08-17 17:55:07 +0900

    relocation conoha-acme-challenge.sh

--- create_conoha_dns_record.sh
+++ create_conoha_dns_record.sh
@@ -1,28 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 
-# -------- #
-# VARIABLE #
-# -------- #
-# ----- certbot ----- #
-# CERTBOT_DOMAIN
-# CERTBOT_VALIDATION
-
-# ----- script ----- # 
-SCRIPT_NAME=$(basename $0)
-SCRIPT_PATH=$(dirname $(readlink -f $0))
-
-# ----- conoha_dns_api.sh  ----- #
-CNH_DNS_DOMAIN=${CERTBOT_DOMAIN}'.'
-CNH_DNS_NAME='_acme-challenge.'${CNH_DNS_DOMAIN}
-CNH_DNS_TYPE="TXT"
-CNH_DNS_DATA=${CERTBOT_VALIDATION}
-
-# -------- #
-# FUNCTION #
-# -------- #
-source ${SCRIPT_PATH}/conoha_dns_api.sh
-
-# ----------------- #
-# CREATE DNS RECORD # 
-# ----------------- #
-create_conoha_dns_record
+if [ -x /usr/libexec/conoha-acme-challenge/conoha-acme-challenge.sh ] ; then
+    /usr/libexec/conoha-acme-challenge/conoha-acme-challenge.sh 
+fi
--- delete_conoha_dns_record.sh
+++ delete_conoha_dns_record.sh
@@ -1,33 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 
-# -------- #
-# VARIABLE #
-# -------- #
-# ----- certbot ----- #
-# CERTBOT_DOMAIN
-# CERTBOT_VALIDATION
-
-# ----- script ----- # 
-SCRIPT_NAME=$(basename $0)
-SCRIPT_PATH=$(dirname $(readlink -f $0))
-
-# ----- conoha_dns_api.sh  ----- #
-CNH_DNS_DOMAIN=${CERTBOT_DOMAIN}'.'
-CNH_DNS_NAME='_acme-challenge.'${CNH_DNS_DOMAIN}
-CNH_DNS_TYPE="TXT"
-CNH_DNS_DATA=${CERTBOT_VALIDATION}
-
-# -------- #
-# FUNCTION #
-# -------- #
-source ${SCRIPT_PATH}/conoha_dns_api.sh
-
-# ------------- #
-# GET RECORD ID #
-# ------------- #
-CNH_RECORD_ID=$(get_conoha_dns_record_id)
-
-# ----------------- #
-# DELETE DNS RECORD #
-# ----------------- #
-delete_conoha_dns_record ${CNH_RECORD_ID}
+if [ -x /usr/libexec/conoha-acme-challenge/conoha-acme-challenge.sh ] ; then
+    /usr/libexec/conoha-acme-challenge/conoha-acme-challenge.sh -r
+fi
